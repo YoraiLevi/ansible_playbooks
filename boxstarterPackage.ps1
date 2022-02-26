3635095d6b3e2a42ba8fbed2d983157eac00bf04
Yorai <Yorai.email@gmail.com>
2022-02-26 20:17:24 +0200

    setup

--- boxstarterPackage.ps1
+++ boxstarterPackage.ps1
@@ -1,5 +1,7 @@
-if (-Not (Get-Command wsl.exe) -or (wsl cat /proc/version | Out-String) -eq (wsl --help | Out-String)){
-    $currentAttempt = 0;
+# wsl --help == wsl --list (no wsl2?) 
+# wsl --list == wsl bashcommand (no repo)
+if (-Not (Get-Command wsl.exe) -or (wsl cat /proc/version | Out-String) -eq (wsl --list | Out-String)){
+    $attempts = Get-Item $env:TEMP\BOXSTARTERATTEMPT* -ErrorAction SilentlyContinue; $currentAttempt = if($attempts.Length){$attempts.Length+1}else{if($attempts){2}else{1}};#New-Item "$env:TEMP\BOXSTARTERATTEMPT$currentAttempt"
     while($currentAttempt -lt 5){
         $attempts = Get-Item $env:TEMP\BOXSTARTERATTEMPT* -ErrorAction SilentlyContinue; $currentAttempt = if($attempts.Length){$attempts.Length+1}else{if($attempts){2}else{1}};New-Item "$env:TEMP\BOXSTARTERATTEMPT$currentAttempt"
         Write-Host "Attempting to install wsl2"
