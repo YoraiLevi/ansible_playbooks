d683394d3c6c6194f726371d53c26a5a0f8beadf
Yorai <Yorai.email@gmail.com>
2022-02-28 22:30:16 +0200

    setup

--- boxstarterPackage.ps1
+++ boxstarterPackage.ps1
@@ -1,7 +1,7 @@
 
-$attempts = Get-Item $env:TEMP\BOXSTARTERATTEMPT* -ErrorAction SilentlyContinue; $currentAttempt = if($attempts.Length){$attempts.Length+1}else{if($attempts){2}else{1}};#New-Item "$env:TEMP\BOXSTARTERATTEMPT$currentAttempt"
-while (($currentAttempt -lt 5) -and (-Not (Get-Command wsl.exe) -or (wsl cat /proc/version | Out-String) -eq (wsl --list | Out-String))){
-    $attempts = Get-Item $env:TEMP\BOXSTARTERATTEMPT* -ErrorAction SilentlyContinue; $currentAttempt = if($attempts.Length){$attempts.Length+1}else{if($attempts){2}else{1}};New-Item "$env:TEMP\BOXSTARTERATTEMPT$currentAttempt"
+$attempts = Get-Item $env:TEMP\BOXSTARTERATTEMPT* -ErrorAction SilentlyContinue; $currentAttempt = if ($attempts.Length) { $attempts.Length + 1 }else { if ($attempts) { 2 }else { 1 } }; #New-Item "$env:TEMP\BOXSTARTERATTEMPT$currentAttempt"
+while (($currentAttempt -lt 5) -and (-Not (Get-Command wsl.exe) -or (wsl cat /proc/version | Out-String) -eq (wsl --list | Out-String))) {
+    $attempts = Get-Item $env:TEMP\BOXSTARTERATTEMPT* -ErrorAction SilentlyContinue; $currentAttempt = if ($attempts.Length) { $attempts.Length + 1 }else { if ($attempts) { 2 }else { 1 } }; New-Item "$env:TEMP\BOXSTARTERATTEMPT$currentAttempt"
     Write-Host "Attempting to install wsl2"
     choco install wsl2 --params "/Version:2 /Retry:true"
     # Restart?
@@ -10,10 +10,14 @@ while (($currentAttempt -lt 5) -and (-Not (Get-Command wsl.exe) -or (wsl cat /pr
 }
 # Delete Attempts after done
 cinst git
-Invoke-Reboot
 cd $env:TEMP; rm "MyFuckingWikiOfEverything" -Force -Recurse -ErrorAction SilentlyContinue
-git clone "https://github.com/YoraiLevi/MyFuckingWikiOfEverything.git"; cd "MyFuckingWikiOfEverything/Ansible"
-.\ExecutePlaybook.ps1 -playbookFile .\playbooks\ping.yml -inventoryFile .\playbooks\inventories\localWindowsWSL\
+try {
+    git clone "https://github.com/YoraiLevi/MyFuckingWikiOfEverything.git"; cd "MyFuckingWikiOfEverything/Ansible"
+    .\ExecutePlaybook.ps1 -playbookFile .\playbooks\ping.yml -inventoryFile .\playbooks\inventories\localWindowsWSL\
+}
+catch{
+    Invoke-Reboot
+}
 # execute theEverything playbook
 
 
