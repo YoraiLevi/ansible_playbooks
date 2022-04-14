e096ecefa1b9cdc5a6f904589c0737ecd0293786
Yorai Levi <yorai.email@gmail.com>
2022-04-14 23:32:05 +0300

    test path

--- automatedSetup.ps1
+++ automatedSetup.ps1
@@ -359,7 +359,9 @@ function Restart-Computer {
     exit
 }
 function Install-Chocolatey {
-    New-Item $PROFILE -ErrorAction SilentlyContinue -Force
+    if (Test-Path $Profile) {
+        New-Item $PROFILE -ErrorAction SilentlyContinue -Force
+    }
     if (-not $env:ChocolateyInstall -or -not (Test-Path "$env:ChocolateyInstall\bin\choco.exe")) {
         Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
         Restart-Computer
